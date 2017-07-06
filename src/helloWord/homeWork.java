c087c541d3c8a73f8d162a0a0a053b5c33f0f9e9
Оля <vitaliyzhernovoy@gmail.com>
2017-07-06 19:34:43 +0300

    first prodjekt

---
+++ src/helloWord/homeWork.java
@@ -0,0 +1,11 @@
+package helloWord;
+
+
+public class homeWork {
+    public static void main(String[] args) {
+        System.out.println("Hello word");
+        System.out.println("еще одна строка текста");
+        System.out.println("и еще одна строка текста");
+
+    }
+}
---
+++ src/vebinags/vebinar1.java
@@ -0,0 +1,13 @@
+package vebinags;
+
+/**
+ * Created by Оля on 06.07.2017.
+ */
+public class vebinar1 {
+    public static void main(String[] args) {
+      System.out.println("Hello word");
+        System.out.println("еще одна строка текста");
+        System.out.println("и еще одна строка текста");
+
+    }
+}
