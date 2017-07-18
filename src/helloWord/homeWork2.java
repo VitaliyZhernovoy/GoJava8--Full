1e03863bdaf4feec4b5c9cf7e095cb623c6447d8
Оля <vitaliyzhernovoy@gmail.com>
2017-07-18 17:41:16 +0300

    немного подправил код

--- src/helloWord/homeWork2.java
+++ src/helloWord/homeWork2.java
@@ -1,7 +1,60 @@
 package helloWord;
 
-/**
- * Created by Оля on 18.07.2017.
- */
+
+import java.util.Scanner;
+
 public class homeWork2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Как Вас зовут?");
+        String name = sc.nextLine();
+
+
+        System.out.println("В каком городе Вы живёте?");
+        String city = sc.nextLine();
+
+
+        int age = 0;
+        while (true) try {
+            System.out.println("Какой Ваш Возраст");
+            age = sc.nextInt();
+            sc.nextLine();
+            break;
+        } catch (Exception e) {
+            System.out.println("Ошибка ввода!");
+            sc.nextLine();
+        }
+
+
+            System.out.println("Какое Ваше Хобби");
+            String hobby = sc.nextLine();
+            System.out.println("");
+            System.out.println("");
+
+            
+
+
+            System.out.println("Вариант 1: ");
+            System.out.println("Имя:     " + name);
+            System.out.println("Город:   " + city);
+            System.out.println("Возраст: " + age);
+            System.out.println("Хобби:   " + hobby);
+            System.out.println("");
+            System.out.println();
+
+            System.out.println("Вариант 2");
+            System.out.println("Человек по имени " + name + " живет в городе " + city);
+            System.out.println("Этому человеку " + age + " лет и любит  " + hobby);
+            System.out.println("");
+            System.out.println();
+
+            System.out.println("Вариант 3");
+            System.out.println(name + " - Имя");
+            System.out.println(city + " - Город проживания");
+            System.out.println(age + " - Возраст");
+            System.out.println(hobby + " - Хобби");
+            System.out.println();
+        }
+
 }
