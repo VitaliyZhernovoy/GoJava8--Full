c30628e84ad76e8ede4b20bcb1252598f340f274
Оля <vitaliyzhernovoy@gmail.com>
2017-07-18 14:39:39 +0300

    Первое дз

--- src/helloWord/homeWork.java
+++ src/helloWord/homeWork.java
@@ -3,9 +3,11 @@ package helloWord;
 
 public class homeWork {
     public static void main(String[] args) {
-        System.out.println("Hello word");
-        System.out.println("еще одна строка текста");
-        System.out.println("и еще одна строка текста");
+        System.out.println("       Как я провёл лето!");
+        System.out.println("Я поехал по работе но обучение в Штудгарт.");
+        System.out.println("через 3 небели переехал в Берлин и оставшееся время провел там с любимой женой.");
+        System.out.println("Ходили по музеям, галлереям, паркам, магазинам, всё было замечательно!!!");
+
 
     }
 }
