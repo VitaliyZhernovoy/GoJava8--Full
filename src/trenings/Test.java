5aa138e53b0ad2fc97e958272783e46cf3100a8e
Оля <vitaliyzhernovoy@gmail.com>
2017-08-05 20:13:31 +0300

    добавил вебинар 3 и начал с ним работать

---
+++ src/trenings/Test.java
@@ -0,0 +1,12 @@
+package trenings;
+
+public class Test {
+    public static void main(String[] args) {
+        int counter = 10;
+        while (counter >= 0) {
+            System.out.println ("Осталось " + counter + " сек.\n");
+            counter--;
+        }
+        System.out.println("BOOM  !!!!!!!!!");
+    }
+}
