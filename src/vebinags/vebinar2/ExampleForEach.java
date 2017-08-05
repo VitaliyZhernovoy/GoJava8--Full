42d768e7788306ef8c80003ceb49779483bcc87c
Оля <vitaliyzhernovoy@gmail.com>
2017-08-05 23:36:58 +0300

    добавил foreach

--- src/vebinags/vebinar2/ExampleForEach.java
+++ src/vebinags/vebinar2/ExampleForEach.java
@@ -1,7 +1,19 @@
 package vebinags.vebinar2;
 
-/**
- * Created by Оля on 05.08.2017.
- */
+
 public class ExampleForEach {
+    public static void main(String[] args) {
+        int[] cars = new int[10];
+
+//        задаем значение для массива
+        for (int i = 0; i < cars.length; i++)
+            cars[i] = i;
+
+//        foreach который может только считывать, выводим в консоль весь массив
+
+        for (int car : cars) {
+            System.out.println(car);
+
+        }
+    }
 }
