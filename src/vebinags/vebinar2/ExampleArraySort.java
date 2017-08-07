c3ece5e7dd78cf53f9baf0936804c99b1ea30b5c
Оля <vitaliyzhernovoy@gmail.com>
2017-08-07 22:04:59 +0300

    разбираюсь с сортировкой пузырьком

--- src/vebinags/vebinar2/ExampleArraySort.java
+++ src/vebinags/vebinar2/ExampleArraySort.java
@@ -1,7 +1,29 @@
 package vebinags.vebinar2;
 
-/**
- * Created by Оля on 05.08.2017.
- */
+
 public class ExampleArraySort {
+    public static void main(String[] args) {
+        int[] cars = new int[] {5, 1, 3, 2, 4,};
+
+// сортировка пузырьком
+
+        for (int i = 0; i < cars.length; i++){
+            for (int j = 1; j < (cars.length -i  ); j++) {
+                if (cars[j-1] > cars[j]) {
+                    int temp = cars[j - 1];
+                    cars[j-1] = cars[j];
+                    cars[j] = temp;
+                }
+
+            }
+            
+        }
+        for (int car : cars) {
+            System.out.println(car);
+            
+        }
+
+
+    }
+
 }
