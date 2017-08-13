60a5a38668d7186b58e4050137db24515a8120da
Оля <vitaliyzhernovoy@gmail.com>
2017-08-13 21:31:51 +0300

    добавление методов в ДЗ№3

--- src/helloWord/homeWork3.java
+++ src/helloWord/homeWork3.java
@@ -1,31 +1,19 @@
 package helloWord;
 
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class homeWork3 {
 
+
     public static void main(String[] args) {
 
-//        Scanner input = new Scanner(System.in); // Объявляем Scanner
-//        System.out.println("Enter array length: ");
-//        int size = input.nextInt(); // Читаем с клавиатуры размер массива и записываем в size
-//        int array[] = new int[size]; // Создаём массив int размером в size
-//        System.out.println("Insert array elements:");
-//    /*Пройдёмся по всему массиву, заполняя его*/
-//        for (int i = 0; i < size; i++) {
-//            array[i] = input.nextInt(); // Заполняем массив элементами, введёнными с клавиатуры
-//        }
-//        System.out.print ("Inserted array elements:");
-//        for (int i = 0; i < size; i++) {
-//            System.out.print (" " + array[i]); // Выводим на экран, полученный массив
-//        }
-//        System.out.println();
-//       }
-//    }
+//
         Scanner input = new Scanner(System.in);
         System.out.println("Введите длину массива: ");
         int size = input.nextInt(); // Читаем с клавиатуры размер массива и записываем в size
+
 
         int[] array = new int[size];
         int i = 0;
@@ -36,20 +24,72 @@ public class homeWork3 {
             System.out.print("Введите " + (i + 1) + "-й элемент массива: ");
             array[i] = in.nextInt();
 
-            System.out.println("вы ввели  " + array[i] );
+            System.out.println("вы ввели  " + array[i]);
 
             i++;
 
         }
+
         System.out.print("Ваш массив: ");
 
         for (int element : array)
 
-
             System.out.print(element + " ");
+        System.out.println("");
+
+
+        int min;
+        int max;
+        int sorting;
+        min = min(array);
+        max = max(array);
+        sorting = sorting(array);
+
+
 
     }
 
 
+    public static int min(int[] array) {
+        int min = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (min > array[i]) min = array[i];
+
+        }
+        System.out.println("Минимальное число массива int = " + min);
+
+        return min;
+
+
+    }
+
+    public static int max(int[] array) {
+        int max = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (max < array[i]) max = array[i];
+
+        }
+        System.out.println("Максимальное число массива int = " + max);
+        return max;
+
+    }
+
+    public static int sorting(int[] array) {
+        int sorting = 0;
+        Arrays.sort(array);
+
+        System.out.println("отсортированный массив = " + Arrays.toString(array));
+        
+        return sorting;
+
+    }
 }
+
+
+
+
+
+
+
+
 
