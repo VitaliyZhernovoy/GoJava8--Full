7753a6e051e725be928e2f264f415e53937a6fb7
Оля <vitaliyzhernovoy@gmail.com>
2017-09-03 22:46:28 +0300

    Пример внутреннего класса

--- src/vebinags/vebinar5_enimals/strucure/furniture/Table.java
+++ src/vebinags/vebinar5_enimals/strucure/furniture/Table.java
@@ -1,7 +1,26 @@
 package vebinags.vebinar5_enimals.strucure.furniture;
 
-/**
- * Created by Оля on 03.09.2017.
- */
+
 public class Table {
+    private int weight;
+    private Glue glue;
+
+    public Table(){
+        this.glue.name = "Клей";
+
+    }
+    //У класса Клей есть доступ к приватным классам Стола,
+    // поскольку он вложен внутрь стола
+    private  class Glue{
+        public String name;
+        public int outDate;
+
+        public void doSomething(){
+            Table.this.weight = 200;
+
+        }
+    }
+
 }
+
+
