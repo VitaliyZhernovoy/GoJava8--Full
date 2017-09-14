9aaacf6b5faec6c5b0662a1db298051a4b9fd5dd
Оля <vitaliyzhernovoy@gmail.com>
2017-09-14 21:25:45 +0300

    ДЗ№5

--- src/helloWord/car_saloon/CarDoor.java
+++ src/helloWord/car_saloon/CarDoor.java
@@ -1,7 +1,40 @@
 package helloWord.car_saloon;
 
-/**
- * Created by Оля on 14.09.2017.
- */
+
 public class CarDoor {
+
+    private int doors;
+    private boolean doorsIsOpen = false;
+
+    private int windows;
+    private boolean windowsIsOpen = false;
+
+   public CarDoor(){
+
+   }
+
+    public  boolean  toggleStateDor(){
+        return doorsIsOpen = !doorsIsOpen;
+
+    }
+    public  boolean  toggleStateWindow(){
+        return windowsIsOpen = !windowsIsOpen;
+
+    }
+    public int Doors() {
+        if (windowsIsOpen = !windowsIsOpen)
+            System.out.println("Дверь закрыли");
+        else
+            System.out.println("Дверь открыли");
+        return Doors();
+    }
+
+    public int Windows(){
+        if (windowsIsOpen = !windowsIsOpen)
+            System.out.println("Окно закрыли");
+        else
+            System.out.println("Окно открыли");
+
+        return Doors();
+    }
 }
--- src/helloWord/car_saloon/Program.java
+++ src/helloWord/car_saloon/Program.java
@@ -1,7 +1,24 @@
 package helloWord.car_saloon;
 
-/**
- * Created by Оля on 14.09.2017.
- */
+
 public class Program {
+    public static void main(String[] args) {
+
+        CarDoor door = new CarDoor();
+
+//        System.out.println("Дверь открыта? "+ door.toggleStateDor());
+//        System.out.println("Дверь открыта? "+ door.toggleStateDor());
+//
+//        System.out.println("Окно открыто?  "+ door.toggleStateWindow());
+//        System.out.println("Окно открыто?  "+ door.toggleStateWindow());
+
+        door.Doors();
+
+        door.Windows();
+
+
+
+
+
+    }
 }
