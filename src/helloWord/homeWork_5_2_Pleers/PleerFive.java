481ec6b5d33d54e5fca523b8ef99ba922334ddb8
Оля <vitaliyzhernovoy@gmail.com>
2017-10-04 12:50:57 +0300

    Дз 5.2 плеер

--- src/helloWord/homeWork_5_2_Pleers/PleerFive.java
+++ src/helloWord/homeWork_5_2_Pleers/PleerFive.java
@@ -1,7 +1,5 @@
 package helloWord.homeWork_5_2_Pleers;
 
-/**
- * Created by Оля on 04.10.2017.
- */
+
 public class PleerFive {
 }
--- src/helloWord/homeWork_5_2_Pleers/PleersProgram.java
+++ src/helloWord/homeWork_5_2_Pleers/PleersProgram.java
@@ -3,5 +3,12 @@ package helloWord.homeWork_5_2_Pleers;
 /**
  * Created by Оля on 04.10.2017.
  */
-public class PleeersProgram {
+public abstract class PleersProgram {
+    private final int price;
+    protected String[] playlist;
+    protected String pleerName;
+
+    protected PleersProgram(int price) {
+        this.price = price;
+    }
 }
