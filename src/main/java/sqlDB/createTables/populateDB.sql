1740e0902c714348e4a4743e34d557d678039e53
Оля <vitaliyzhernovoy@gmail.com>
2018-05-24 11:51:37 +0300

    создание связей согласно требованию!

--- src/main/java/sqlDB/createTables/populateDB.sql
+++ src/main/java/sqlDB/createTables/populateDB.sql
@@ -6,9 +6,9 @@ insert into DEVELOPERS (developer_name, age, sex) VALUES
   ('Masha',21,'female');
 
 insert into SKILLS (id, skill, grade) VALUES
-  (1,'Java','junior'),
-  (2,'Java','middle'),
-  (3,'Java','senior');
+  (1,'Java','Junior Data Science Engineer'),
+  (2,'Java','Middle Java Full Stack Engineer'),
+  (3,'Java','Senior Java Developer');
 
 insert into COMPANIES (company_name, adress) VALUES
   ('Luxoft','Kiev'),
@@ -17,10 +17,53 @@ insert into COMPANIES (company_name, adress) VALUES
 
 insert into PROJECTS (project_name, description) VALUES
   ('StarDefender','MMO Game'),
-  ('IMS searching engine','some business logic'),
+  ('SMART Academy','some business logic'),
   ('Free Driver','Auto - Pilot system'),
   ('The Witcher','Game');
 
 insert into CUSTOMERS (customer_name, adress) VALUES
   ('World owner','whole world');
 
+insert into COMPANIES_support_CUSTOMERS (companie_id, customer_id) VALUES
+  (1,1),
+  (2,1),
+  (3,1);
+
+insert into COMPANIES_support_PROJECTS (company_id, project_id) VALUES
+  (1,2),
+  (2,2),
+  (3,1),
+  (4,3),
+  (5,4);
+
+insert into CUSTOMERS_support_PROJECTS (customer_id, project_id) VALUES
+  (1,1),
+  (1,2),
+  (1,3),
+  (1,4);
+
+insert into DEVELOPERS_support_COMPANIES (developer_id, company_id) VALUES
+  (1,1),
+  (2,2),
+  (3,3),
+  (4,2),
+  (5,3),
+  (1,2),
+  (2,3),
+  (3,1),
+  (4,1),
+  (5,2);
+
+insert into DEVELOPERS_support_PROJECTS (developer_id, project_id) VALUES
+  (1,1),
+  (2,1),
+  (3,2),
+  (4,3),
+  (5,4);
+
+insert into DEVELOPERS_support_SKILLS (developer_id, skill_id) VALUES
+  (1,1),
+  (2,1),
+  (3,2),
+  (4,2),
+  (5,3);
